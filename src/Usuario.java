779775b06b81770ab042a82a1eebbc390d005d52
Jon <jonjcosta@gmail.com>
2012-06-20 20:57:17 -0300

    20/06 - 20:56 - Adicionada classe Usuario e excluido NewFile

---
+++ src/Usuario.java
@@ -0,0 +1,209 @@
+
+public class Usuario {
+	
+	private int idcliente, situacao ;
+	private String cpf, nome, nomepai, nomemae, tipodoc, numdoc, emissor, naturalidade, nacionalidade ;
+	private String estadocivil, conjuge, capacidadecivil, numinss, formarecebinss, ocupacao, nivelcargo ;
+	private String empregador, nomeUsuario, senhaUsuario, tipoUsuario ;
+	private float rendamensal ;
+	private char sexo ;
+	
+	public Usuario(){
+		
+	}
+
+	public int getIdcliente() {
+		return idcliente;
+	}
+
+	public void setIdcliente(int idcliente) {
+		this.idcliente = idcliente;
+	}
+
+	public int getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(int situacao) {
+		this.situacao = situacao;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomepai() {
+		return nomepai;
+	}
+
+	public void setNomepai(String nomepai) {
+		this.nomepai = nomepai;
+	}
+
+	public String getNomemae() {
+		return nomemae;
+	}
+
+	public void setNomemae(String nomemae) {
+		this.nomemae = nomemae;
+	}
+
+	public String getTipodoc() {
+		return tipodoc;
+	}
+
+	public void setTipodoc(String tipodoc) {
+		this.tipodoc = tipodoc;
+	}
+
+	public String getNumdoc() {
+		return numdoc;
+	}
+
+	public void setNumdoc(String numdoc) {
+		this.numdoc = numdoc;
+	}
+
+	public String getEmissor() {
+		return emissor;
+	}
+
+	public void setEmissor(String emissor) {
+		this.emissor = emissor;
+	}
+
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getEstadocivil() {
+		return estadocivil;
+	}
+
+	public void setEstadocivil(String estadocivil) {
+		this.estadocivil = estadocivil;
+	}
+
+	public String getConjuge() {
+		return conjuge;
+	}
+
+	public void setConjuge(String conjuge) {
+		this.conjuge = conjuge;
+	}
+
+	public String getCapacidadecivil() {
+		return capacidadecivil;
+	}
+
+	public void setCapacidadecivil(String capacidadecivil) {
+		this.capacidadecivil = capacidadecivil;
+	}
+
+	public String getNuminss() {
+		return numinss;
+	}
+
+	public void setNuminss(String numinss) {
+		this.numinss = numinss;
+	}
+
+	public String getFormarecebinss() {
+		return formarecebinss;
+	}
+
+	public void setFormarecebinss(String formarecebinss) {
+		this.formarecebinss = formarecebinss;
+	}
+
+	public String getOcupacao() {
+		return ocupacao;
+	}
+
+	public void setOcupacao(String ocupacao) {
+		this.ocupacao = ocupacao;
+	}
+
+	public String getNivelcargo() {
+		return nivelcargo;
+	}
+
+	public void setNivelcargo(String nivelcargo) {
+		this.nivelcargo = nivelcargo;
+	}
+
+	public String getEmpregador() {
+		return empregador;
+	}
+
+	public void setEmpregador(String empregador) {
+		this.empregador = empregador;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public float getRendamensal() {
+		return rendamensal;
+	}
+
+	public void setRendamensal(float rendamensal) {
+		this.rendamensal = rendamensal;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+	
+}
